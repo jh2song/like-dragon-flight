2b1fc685bfca91c563dd8735764f75309bc85f57
Jihoon Song <spec0003@naver.com>
2023-11-28 11:15:41 +0900

    Feat: 레벨UI 연동
    TODO
    강의를 다시 보고 제작하기

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.Mathematics;
 using UnityEngine;
 
@@ -15,17 +16,23 @@ public class GameManager : MonoBehaviour
 
     [SerializeField]
     private int _level = 1;
+
+    private TextMeshProUGUI _levelTmp;
     
     // Start is called before the first frame update
     void Start()
     {
         BulletsContainer = GameObject.Find("Bullets") ?? new GameObject("Bullets");
         InvokeRepeating("GenerateBullet", 0f, _bulletInteval);
+
+        _levelTmp = GameObject.Find("LevelTxt").GetComponent<TextMeshProUGUI>();
+        _levelTmp.text = $"Level {_level}";
     }
 
     // Update is called once per frame
     void Update()
     {
+        
     }
 
     void GenerateBullet()
