14a6923270a7725a4e60c9a061f8db19b3d17b03
Jihoon Song <spec0003@naver.com>
2023-12-08 08:33:44 +0900

    Feat: 적선 미사일 발사 구현
    TODO
    적선을 Instantiate 해야함

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public GameObject EnemyBullet;
+
+    private float _bulletInteval = 0.5f;
     
     private void Start()
     {
-        
+        InvokeRepeating("GenerateBullet", 0f, _bulletInteval);
     }
 
     private void Update()
     {
-        
+    }
+    
+    private void GenerateBullet()
+    {
+        GameObject newBullet = Instantiate(EnemyBullet, Vector3.zero, Quaternion.Euler(0f, 0f, 270f), gameObject.transform);
+        newBullet.transform.position = gameObject.transform.position + Vector3.down;
     }
 }
